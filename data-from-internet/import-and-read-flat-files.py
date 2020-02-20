ebaa35d32f31f7c641b9fb0eae0c4ffe309ee36c
Colin Fendrick <colin.fendrick@gmail.com>
2020-02-20 06:13:32 -0500

    :sparkles: Handling data from the internet pt1

---
+++ data-from-internet/import-and-read-flat-files.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+url = 'https://s3.amazonaws.com/assets.datacamp.com/production/course_1606/datasets/winequality-red.csv'
+
+# Read file into a DataFrame: df
+df = pd.read_csv(url, sep=';')
+print(df.head())
+
+# Plot first column of df
+pd.DataFrame.hist(df.ix[:, 0:1])
+plt.xlabel('fixed acidity (g(tartaric acid)/dm$^3$)')
+plt.ylabel('count')
+plt.show()
